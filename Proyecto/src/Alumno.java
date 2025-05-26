965405b02d6ac80e6fb238589651ac68fc8f225b
DelfiLinares <166736456+DelfiLinares@users.noreply.github.com>
2025-05-26 10:36:04 -0300

    Add files via upload

--- Proyecto/src/Alumno.java
+++ Proyecto/src/Alumno.java
@@ -1,61 +1,37 @@
+package colegio;
+
+import general.Persona;
+
+import java.time.LocalDate;
 import java.util.ArrayList;
 
-public class Alumno {
-
-        //nombre, apellido, fechaNacimiento, listaDeNotas
-        private String nombre;
-        private String apellido;
-        private Fecha fechaNacimiento;
+public class Alumno extends Persona {
+        private String division;
         private ArrayList<Integer> notas;
         private ArrayList<Materia>materia;
 
 
-        public Alumno(String nombre, String apellido, Fecha fechaNacimiento, ArrayList<Integer>notas, ArrayList<Materia>materia) {
-            this.nombre=nombre;
-            this.apellido=apellido;
-            this.fechaNacimiento=fechaNacimiento;
+        public Alumno(String nombre, String apellido, LocalDate fechaNacimiento, String division,ArrayList<Integer>notas, ArrayList<Materia>materia) {
+           super(nombre, apellido, fechaNacimiento);
+            this.division=division;
             this.notas=notas;
             this.materia=materia;
-        }
-        public Alumno(String nombre, Fecha fechaNacimiento, ArrayList<Integer>notas){
-            this.nombre=nombre;
-            this.fechaNacimiento=fechaNacimiento;
+
+
+    }
+        public Alumno(String nombre, LocalDate fechaNacimiento, ArrayList<Integer>notas){
+            super(nombre, fechaNacimiento);
             this.notas=notas;
         }
 
         public Alumno(){
-            this.nombre="Hector";
-            this.apellido="Lesertesseur";
-            this.fechaNacimiento=new Fecha(1,3,2024);
+         super();
+            this.division="2B";
             this.notas=new ArrayList<Integer>();
             this.materia=new ArrayList<Materia>();
             notas.add(3);
             notas.add(9);
             notas.add(7);
-        }
-
-        public String getNombre() {
-            return nombre;
-        }
-
-        public void setNombre(String nombre) {
-            this.nombre = nombre;
-        }
-
-        public String getApellido() {
-            return apellido;
-        }
-
-        public void setApellido(String apellido) {
-            this.apellido = apellido;
-        }
-
-        public Fecha getFechaNacimiento() {
-            return fechaNacimiento;
-        }
-
-        public void setFechaNacimiento(Fecha fechaNacimiento) {
-            this.fechaNacimiento = fechaNacimiento;
         }
 
         public ArrayList<Integer> getNotas() {
@@ -109,7 +85,7 @@ public class Alumno {
                 listaContenidos.add("Present perfect");
 
                 ArrayList<Alumno>alumnosInscriptos=new ArrayList<Alumno>();
-                alumnosInscriptos.add(new Alumno("Delfina", "Lithium", new Fecha(2, 6, 2008), new ArrayList<Integer>());
+               // alumnosInscriptos.add(new colegio.Alumno("Delfina", "Lithium", new Fecha(2, 6, 2008), new ArrayList<Integer>());
 
                 Materia mat= new Materia("Ingles", listaContenidos,alumnosInscriptos);
 
@@ -120,7 +96,7 @@ public class Alumno {
                 ArrayList<Materia>materias=new ArrayList<Materia>();
                 materias.add(new Materia());
 
-                Alumno a1=new Alumno("Carlita","Wolf",new Fecha(1,2,2023), notas, materias);
+                Alumno a1=new Alumno("Carlita","Wolf",LocalDate.of(1,2,2023),"3A", notas, materias);
                 System.out.println("Mayor nota de "+a1.getNombre() +" es: "+a1.mayorNota(a1));
                 System.out.println("Menor nota de "+a1.getNombre() +" es: "+a1.menorNota(a1));
                 a1.agregarNota(4);
