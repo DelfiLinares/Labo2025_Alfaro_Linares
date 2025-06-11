353f4e4a6cb7d11bd6965be70e0818a3f7c0ec3b
vikitoriaalfaro <victoriaalfarofernandez@gmail.com>
2025-06-11 09:06:41 -0300

    ejs hechos hasta el 4

---
+++ Proyecto/src/receta/Plataforma.java
@@ -0,0 +1,93 @@
+package receta;
+
+import tienda_electronica.Producto;
+
+import java.time.LocalTime;
+import java.util.ArrayList;
+
+public class Plataforma {
+    ArrayList<Plato> recetas;
+
+    public Plataforma(ArrayList<Plato> recetas) {
+        this.recetas = recetas;
+    }
+
+    public Plataforma() {
+        this.recetas = new ArrayList<>();
+        recetas.add(new platoPrincipal());
+    }
+
+    public ArrayList<Plato> getRecetas() {
+        return recetas;
+    }
+
+    public void setRecetas(ArrayList<Plato> recetas) {
+        this.recetas = recetas;
+    }
+
+    public void agregar(Plato p1) {
+        this.recetas.add(p1);
+    }
+
+    public void eliminar(Plato p1) {
+        this.recetas.remove(p1);
+    }
+
+    public void modificar(Plato p1, Plato nuevo) {
+        for (Plato aux : recetas) {
+            if (aux == p1) {
+                aux = nuevo;
+            }
+        }
+    }
+
+    public void dificultad(Nivel n){
+        System.out.println("----Los platos con nivel:"+n.name());
+        for(Plato p: recetas){
+            if (p.nivel.equals(n)){
+                System.out.println(p.getNombre());
+            }
+        }
+    }
+    public Plato mayorCantidadPasos(){
+        for(Plato p: recetas){
+
+        }
+    }
+
+    public void porTipo(String tipo){
+        System.out.println("----Búsqueda por tipo:"+tipo);
+        String t="class receta."+tipo;
+        for(Plato p: recetas){
+            String aux=p.getClass().toString();
+            if(aux.equals(t)){
+              System.out.println(p.getNombre());
+            }
+        }
+    }
+
+
+    public int cantidad(){
+        return recetas.size();
+    }
+    public static void main(String[] args) {
+        ArrayList<String>pasos=new ArrayList<>();
+        pasos.add("Paso 1");
+        pasos.add("Paso 2");
+        pasos.add("Paso 3");
+        pasos.add("Paso 5");
+        Entrada e=new Entrada("Rabas",Nivel.MEDIO, pasos, Temperatura.CALIENTE);
+        platoPrincipal pp =new platoPrincipal("Milanesa a caballo", Nivel.FACIL, pasos, LocalTime.of(0,30,32), 3);
+        Postre po=new Postre("Torta Oreo",Nivel.FACIL, pasos, -18, false);
+        ArrayList<Plato>recetas=new ArrayList<>();
+        recetas.add(e);
+        recetas.add(pp);
+        recetas.add(po);
+
+        Plataforma p1=new Plataforma(recetas);
+        Nivel nivelazo=Nivel.MEDIO;
+        p1.dificultad(nivelazo);
+        p1.porTipo("Postre");
+        System.out.println("Cantidad de recetas:"+p1.cantidad());
+    }
+}
