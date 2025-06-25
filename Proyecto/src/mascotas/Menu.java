8cbcd55ba2c44d9c769b534d88d8cbbb2f8e2b72
vikitoriaalfaro <victoriaalfarofernandez@gmail.com>
2025-06-25 09:55:57 -0300

    sin metodo que elimina peces

--- Proyecto/src/mascotas/Menu.java
+++ Proyecto/src/mascotas/Menu.java
@@ -3,28 +3,14 @@ package mascotas;
 import java.util.ArrayList;
 
 public class Menu {
-    ArrayList<Mascota>mascotas;
-
-    public Menu(ArrayList<Mascota>mascotas){
-        this.mascotas=mascotas;
-    }
-
-    public ArrayList<Mascota> getMascotas() {
-        return mascotas;
-    }
-
-    public void setMascotas(ArrayList<Mascota> mascotas) {
-        this.mascotas = mascotas;
-    }
-
-    public void alta(Mascota m1){
+    public void alta(Mascota m1, ArrayList<Mascota>mascotas){
         mascotas.add(m1);
     }
 
-    public void eliminar(Mascota m1){
+    public void eliminar(Mascota m1, ArrayList<Mascota>mascotas){
         mascotas.remove(m1);
     }
-    public void modificar(Mascota m1, Mascota mNueva){
+    public void modificar(Mascota m1, Mascota mNueva, ArrayList<Mascota>mascotas){
         for (Mascota aux : mascotas) {
             if (aux == m1) {
                 aux = mNueva;
@@ -32,19 +18,34 @@ public class Menu {
         }
     }
 
-    public void saludoDuenio(Mascota m, Duenio d){
-        m.saludo(d);
+    public void actualizacion(ArrayList<Mascota>mascotas){
+        for(Mascota aux: mascotas){
+            if(aux instanceof Pez){
+                //para eliminar a los peces sin vida del array
+            }
+        }
     }
+
     public static void main(String[] args) {
         Perro p1=new Perro("Nubebecito",new Duenio("Delfi","Linares"), Animal.PERRO, 9);
-        Pez pz1=new Pez("Darwin",new Duenio("Cande","Malempré"), Animal.PEZ,4);
+        Pez pz1=new Pez("Darwin",new Duenio("Cande","Malempre"), Animal.PEZ,7);
         Pajaro pj1=new Pajaro("Lili",new Duenio("Aby","Tedisk"),true, Animal.PAJARO, 7);
         ArrayList<Mascota>mascotas=new ArrayList<>();
         mascotas.add(p1);
         mascotas.add(pz1);
         mascotas.add(pj1);
-        Menu m1=new Menu(mascotas);
+        pz1.saludoDuenio(new Duenio("Cande","Malempre"));
+        pz1.alimentar();
+        pz1.saludoDuenio(new Duenio("Cande","Matambre"));
+        pz1.clase();;
 
+        p1.saludoDuenio(new Duenio("Delfi","Linares"));
+        p1.alimentar();
+        p1.saludoDuenio(new Duenio("Delfi","Lampara"));
+
+        pj1.saludoDuenio(new Duenio("Aby", "Tedisk"));
+        pj1.alimentar();
+        pj1.saludoDuenio(new Duenio("Aby", "Tedesco"));
 
     }
 }
